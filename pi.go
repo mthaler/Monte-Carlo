5b8f0871a2f54218e6d99ff78caf2cd30aafc275
Michael Thaler <michael.thaler@dlr.de>
2025-08-30 09:57:45 +0200

    Updated pi.go.

--- pi.go
+++ pi.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"math"
+	"math/rand"
+)
+
 func calculatePi() float64 {
+	x := rand.Float64()
+	y := rand.Float64()
+	x = x/math.MaxFloat64*2.0 + 1.0
+	y = y/math.MaxFloat64*2.0 + 1.0
+
 	return 0.0
 }
